734ca3ad9119146515523b3c24d19051ba708ade
Allan Wang <allan@getbarti.com>
2023-06-06 23:10:24 -0700

    upgrade dependencies to latest

--- src/app.py
+++ src/app.py
@@ -1,14 +1,15 @@
 from flask import Flask
+from src.extensions import db
 from src.endpoints import home
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
-    from src.extensions import db
     db.init_app(app)
     # We are doing a create all here to set up all the tables. Because we are using an in memory sqllite db, each
     # restart wipes the db clean, but does have the advantage of not having to worry about schema migrations.
-    db.create_all(app=app)
+    with app.app_context():
+        db.create_all()
     app.register_blueprint(home)
     return app
